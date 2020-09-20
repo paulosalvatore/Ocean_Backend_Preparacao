39b5b9634230ee28a97714d3ee286c3ee75bdea4
Paulo Salvatore <paulo@salvatore.tech>
2020-09-20 17:54:51 -0300

    Messages local CRUD

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { v4 as uuidv4 } from 'uuid';
+
 const app = express();
 
-import { v4 as uuidv4 } from 'uuid';
+const port = process.env.PORT || 3000;
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
+/*
 
 // (async () => {
 
-/*
+/!*
 // Retrieve
 const MongoClient = require('mongodb').MongoClient;
 
@@ -27,33 +35,100 @@ const { insertedCount } = await collection.insertOne({
 const result = await collection.countDocuments({});
 
 console.log(result);
+*!/
+
+})();
 */
 
-const messages = [];
+const messages = {};
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
+// CREATE
 app.post('/messages', (req, res) => {
     const message = req.body;
+    console.log(req.body);
 
-    if (!message.text) {
+    if (!message || !message.text) {
         return res.status(400).json({ message: `Message text is empty or wasn't found` });
-    } else if (messages.find(msg => msg.text === message.text)) {
+    } else if (Object.values(messages).find(msg => msg.text === message.text)) {
         return res.status(409).json({ message: `Message is duplicated.` });
     }
 
     const id = uuidv4();
 
     messages[id] = {
+        id,
         text: message.text,
     };
 
-    return res.status(202).json({ message: 'Message added successfully.' });
+    return res.status(202).json(messages[id]);
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+// READ ALL
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
 
-// })();
+// READ BY ID
+app.get('/messages/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!messages[id]) {
+        return res.status(404).send({ message: `Message with id '${id}' wasn't found.` });
+    }
+
+    res.send(messages[id]);
+});
+
+// UPDATE
+app.put('/messages/:id', (req, res) => {
+    const id = req.params.id;
+
+    const message = req.body;
+
+    if (!messages[id]) {
+        return res.status(404).send({ message: `Message with id '${id}' wasn't found.` });
+    } else if (Object.values(messages).find(msg => msg.text === message.text)) {
+        return res.status(409).json({ message: `Message is duplicated.` });
+    }
+
+    messages[id] = {
+        id,
+        text: message.text,
+    };
+
+    res.send({ message: `Message with id '${id}' updated successfully.` });
+});
+
+app.delete('/messages:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!messages[id]) {
+        return res.status(404).send({ message: `Message with id '${id}' wasn't found.` });
+    }
+
+    delete messages[id];
+
+    res.send({ message: `Message with id '${id}' deleted successfully.` });
+});
+
+// DELETE
+
+// Error handling
+
+app.use((req, res, next) => {
+    const error = new Error(`Endpoint wasn't found`);
+    error.status = 404;
+    next(error);
+});
+
+// error handler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || 'Internal Server Error',
+        },
+    });
+});
+
+app.listen(port, () => console.log(`App listening on port: ${port}`));
