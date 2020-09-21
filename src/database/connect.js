d126b61303bb790bc5708ec379add6629e26f8e3
Paulo Salvatore <paulo@salvatore.tech>
2020-09-21 13:24:54 -0300

    Added cloud url

--- src/database/connect.js
+++ src/database/connect.js
@@ -3,11 +3,14 @@ import mongodb from 'mongodb';
 export default async () => {
     console.info('Connecting to MongoDB database...');
 
-    const client = await mongodb.MongoClient.connect('mongodb://localhost:27017/',
+    const urlLocal = 'mongodb://localhost:27017/';
+    const urlCloud = 'mongodb+srv://admin:Uu8ftSpPxbyJiNk@ocean-preparacao-backen.0oe98.mongodb.net/ocean-preparacao?retryWrites=true&w=majority';
+
+    const client = await mongodb.MongoClient.connect(urlCloud,
         { useUnifiedTopology: true },
     );
 
-    console.log('MongoDB connected');
+    console.info('MongoDB connected');
 
     return client.db('ocean-preparacao');
 };
